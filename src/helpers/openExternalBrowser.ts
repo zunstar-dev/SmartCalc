742351acd991a44a96d81bebb35e5be1f59d0b85
zunstar <zunstardev@gmail.com>
2024-06-22 10:39:50 +0900

    edit : 인앱브라우저 체크

--- src/helpers/openExternalBrowser.ts
+++ src/helpers/openExternalBrowser.ts
@@ -4,23 +4,11 @@ export const openExternalBrowser = () => {
 
   const isIOS = () => /iPhone|iPad|iPod/i.test(navigator.userAgent);
   const isAndroid = () => /Android/i.test(navigator.userAgent);
-  const isKakaoBrowser = () => /KAKAOTALK/i.test(navigator.userAgent);
+  const isKakaoBrowser = () => /kakaotalk/i.test(navigator.userAgent);
 
   if (isKakaoBrowser()) {
     if (isIOS()) {
-      alert(
-        'URL주소가 복사되었습니다.\n\nSafari가 열리면 주소창을 길게 터치한 뒤, "붙여놓기 및 이동"을 누르면 정상적으로 이용하실 수 있습니다.'
-      );
-      const copyToClipboard = (val: string) => {
-        const t = document.createElement('textarea');
-        document.body.appendChild(t);
-        t.value = val;
-        t.select();
-        document.execCommand('copy');
-        document.body.removeChild(t);
-      };
-      copyToClipboard(url);
-      window.location.href = 'x-web-search://?';
+      window.location.href = `kakaotalk://web/openExternal?url=${encodeURIComponent(url)}`;
     } else if (isAndroid()) {
       window.location.href = `intent://${url.replace(/^https?:\/\//, '')}#Intent;scheme=https;package=com.android.chrome;end`;
     } else {
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { registerSW } from 'virtual:pwa-register';
 import AppProviders from './AppProviders';
+import { openExternalBrowser } from './helpers/openExternalBrowser';
+
+// 인앱 (카카오 브라우저 대응)
+openExternalBrowser();
+
 // PWA 플러그인 서비스 워커 등록
 registerSW({ immediate: true });
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
